7ecd626aeb293e3d50d9a1e22965ca25767ba269
291207 <99871513+291207@users.noreply.github.com>
2024-05-31 13:24:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,6 +67,7 @@ USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 ADMINS.append(OWNER_ID)
 ADMINS.append(7170648639)
 ADMINS.append(5674921587)
+ADMINS.append(6937142376)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
