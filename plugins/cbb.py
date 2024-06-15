a1ed8bbbab6da00bc2d1ef030fd928107dbecbc3
291207 <99871513+291207@users.noreply.github.com>
2024-06-15 22:14:27 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -2,7 +2,7 @@
 
 from pyrogram import __version__
 from bot import Bot
-from config import OWNER_ID
+from config import OWNER_ID, PersonId
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 @Bot.on_callback_query()
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://www.gooogle.com'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://www.gooogle.com'>Click Here</a>\n○ Channel : @bot_list_hub\n○ Support Group : @botlistdisc</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={PersonId}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://www.gooogle.com'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://www.gooogle.com'>Click Here</a>\n○ Channel : @bot_list_hub\n○ Support Group : @botlistdisc</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
