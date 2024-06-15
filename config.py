4b84e0fcc52f334640d8b34e4100b6999887bf94
291207 <99871513+291207@users.noreply.github.com>
2024-06-15 22:33:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -70,8 +70,6 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append()
-ADMINS.append()
 ADMINS.append(5674921587)
 ADMINS.append(6937142376)
 
